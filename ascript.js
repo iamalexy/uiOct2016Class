a95e4b2702cfa690b2108c5358b964c5415b3971
aky <aky.yar@gmail.com>
2016-10-10 22:21:55 -0500

    updating fork

---
+++ ascript.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+	<head>
+		<title>BHANU</title>
+	</head>
+
+	<body>
+		<p>By using JavaScript we can change the HTML content</p>
+		<p id="demo"> This is Bhanu</p>
+		<button type="button" onClick="document.getElementById('demo').innerhtml="This is alekhya">Click on me</button>
+	</body>
+</html>
