2282fa8ca78bb72dfd83d86184d0514dad7c692d
aky <aky.yar@gmail.com>
2016-10-31 03:40:25 -0500

    submit work

---
+++ jaScr.js
@@ -0,0 +1,15 @@
+function myFun() {
+				document.getElementById('demo').innerHTML="change the text";
+				// body...
+			}
+
+
+window.alert("Alekhya");
+
+//document.write(5+6);
+console.log("Alekhya"+"  "+"Bhanu");
+
+var x=10;
+var y=20;
+var z= x+y;
+document.getElementById('demo').innerHTML=z';
---
+++ jascr1.js
@@ -0,0 +1,4 @@
+function myFun() {
+				document.getElementById('demo').innerHTML="change the text";
+				// body...
+			}
